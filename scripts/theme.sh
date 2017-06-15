f99e84cec5cfcab7dc6c9e2847b180a79db10183
Thomas Pink <thomas.pink@me.com>
2017-06-15 14:00:00 +0100

    theme copy script

--- scripts/theme.sh
+++ scripts/theme.sh
@@ -3,21 +3,49 @@
 set -u -e -o pipefail
 
 PUBLIC=`pwd`/public
-THEME_SRC=`pwd`/src/theme
 THEMES=${PUBLIC}/wp-content/themes
+THEME_SRC=`pwd`/src/theme
+THEME_DEST=${THEMES}/sls-2017
+DEV=false
 
-rm -rf ${THEMES}/sls-2017
-mkdir ${THEMES}/sls-2017
+for ARG in "$@"; do
+  case "$ARG" in
+    --dev=*)
+      DEV=true
+      ;;
+    *)
+      echo 'Unknown option $ARG.'
+      exit 1
+      ;;
+  esac
+done
+
+
+#######################################
+# Copies a file or folder
+# Arguments:
+#   param1 - File or folder to copy
+#   param2 - Destination
+# Returns:
+#   None
+#######################################
+copyFile() {
+  if [[ -d ${1} ]]; then
+    cp -R ${1} ${2}
+  elif [[ -f ${1} ]]; then
+      cp ${1} ${2}
+  fi
+}
+
+rm -rf $THEME_DEST
+mkdir $THEME_DEST
 
 FILES=${THEME_SRC}/*
 for f in $FILES
 do
-  if [[ -d $f ]]; then
-    echo "$f is a folder"
-  elif [[ -f $f ]]; then
-      echo "$f is a file"
+  if [[ ${DEV} == true ]]; then
+    echo '1';
   else
-      echo "$f is not valid"
-      exit 1
+      copyFile $f $THEME_DEST
   fi
 done
