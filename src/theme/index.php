d1e516f315fc6f57a6b757a73b613874100ff297
Thomas Pink <thomas.pink@me.com>
2017-08-08 16:53:19 +0200

    formatting

--- src/theme/index.php
+++ src/theme/index.php
@@ -25,31 +25,24 @@
 </head>
 <body <?php body_class(); ?>>
   <nav class="skiplinks" role="navigation">
-    <a href="#main-navigation" class="visuallyhidden focusable"><?php _e( 'Zur Hauptnavigation springen', 'sls2017' ); ?></a>
-    <a href="#main" class="visuallyhidden focusable"><?php _e( 'Zum Seiteninhalt springen', 'sls2017' ); ?></a>
+    <a href="#main-navigation" class="visuallyhidden focusable"><?php _e( 'Zur Hauptnavigation springen', 'sls-2017' ); ?></a>
+    <a href="#main" class="visuallyhidden focusable"><?php _e( 'Zum Seiteninhalt springen', 'sls-2017' ); ?></a>
   </nav>
   <div class="root">
     <?php get_template_part( 'template-parts/header/header', '' ); ?>
     <main id="main" role="main">
       <?php
-			if ( have_posts() ) :
-
-				/* Start the Loop */
-				while ( have_posts() ) : the_post();
-
-					/*
-					 * Include the Post-Format-specific template for the content.
-					 * If you want to override this in a child theme, then include a file
-					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-					 */
-					// get_template_part( 'template-parts/post/content', get_post_format() );
-
-				endwhile;
+      if ( have_posts() ) :
+        /* Start the Loop */
+        while ( have_posts() ) : the_post();
+          // Include the Post-Format-specific template for the content.
+          // If you want to override this in a child theme, then include a file
+          // called content-___.php (where ___ is the Post Format name) and that will be used instead.
+          // get_template_part( 'template-parts/post/content', get_post_format() );
+        endwhile;
       else :
-
-				get_template_part( 'template-parts/post/content', 'none' );
-
-			endif;
+        get_template_part( 'template-parts/post/content', 'none' );
+      endif;
       ?>
     </main>
   </div>
