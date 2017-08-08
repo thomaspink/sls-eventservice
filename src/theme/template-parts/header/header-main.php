14452634b2fc47026c74c4c6d030bfafbaca4d95
Thomas Pink <thomas.pink@me.com>
2017-08-08 17:10:05 +0200

    added header menu

--- src/theme/template-parts/header/header-main.php
+++ src/theme/template-parts/header/header-main.php
@@ -4,14 +4,13 @@
       <?php get_template_part( 'template-parts/header/header', 'logo' ); ?>
     </a>
     <button class="toggle-drawer"><i class="material-icons">menu</i></button>
-    <nav class="main-nav" id="main-navigation" aria-label="Hauptnavigation" role="navigation">
-      <ul class="main-nav__list">
-        <li class="main-nav__item"><a href="/leistungen" class="main-nav__link">Leistungen</a></li>
-        <li class="main-nav__item"><a href="/unternehmen" class="main-nav__link">Unternehmen</a></li>
-        <li class="main-nav__item active"><a href="/referenzen" class="main-nav__link">Referenzen</a></li>
-        <li class="main-nav__item"><a href="/gebrauchtboerse" class="main-nav__link">Gebrauchtbörse</a></li>
-        <li class="main-nav__item"><a href="/kontakt" class="main-nav__link">Kontakt</a></li>
-      </ul>
-    </nav>
+    <?php wp_nav_menu( array(
+      'theme_location' => 'top',
+      'fallback_cb' => false,
+      'menu_class' => 'main-nav__list',
+      'container' => 'nav',
+      'container_id' => 'main-navigation',
+      'container_class' => 'main-nav',
+    )); ?>
   </div>
 </div>
