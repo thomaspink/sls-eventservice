fd0b311bf07c65869bc93a01eeec554967b2eda2
Thomas Pink <thomas.pink@me.com>
2017-08-11 14:00:00 +0100

    drawer nav

--- src/theme/template-parts/drawer/drawer.php
+++ src/theme/template-parts/drawer/drawer.php
@@ -1,11 +1,9 @@
 <side-drawer aria-hidden="true">
-  <nav class="side-drawer__nav">
-    <ul class="side-drawer__list">
-      <li class="side-drawer__item"><a href="/leistungen" class="side-drawer__link">Leistungen</a></li>
-      <li class="side-drawer__item"><a href="/unternehmen" class="side-drawer__link">Unternehmen</a></li>
-      <li class="side-drawer__item"><a href="/referenzen" class="side-drawer__link">Referenzen</a></li>
-      <li class="side-drawer__item"><a href="/gebrauchtboerse" class="side-drawer__link">Gebrauchtbörse</a></li>
-      <li class="side-drawer__item"><a href="/kontakt" class="side-drawer__link">Kontakt</a></li>
-    </ul>
-  </nav>
+  <?php wp_nav_menu( array(
+      'theme_location' => 'top',
+      'fallback_cb' => false,
+      'menu_class' => 'side-drawer__list',
+      'container' => 'nav',
+      'container_class' => 'side-drawer__nav',
+    )); ?>
 </side-drawer>
