e2d5427c0819a4fa2bc5a1ffe365856d5835c32b
Thomas Pink <thomas.pink@me.com>
2017-08-12 14:43:30 +0200

    content

--- src/theme/index.php
+++ src/theme/index.php
@@ -39,6 +39,7 @@
           // If you want to override this in a child theme, then include a file
           // called content-___.php (where ___ is the Post Format name) and that will be used instead.
           // get_template_part( 'template-parts/post/content', get_post_format() );
+          get_template_part( 'template-parts/post/content', 'none' );
         endwhile;
       else :
         get_template_part( 'template-parts/post/content', 'none' );
