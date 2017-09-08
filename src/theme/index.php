946ddb7a966881a1f1bed27798d467527cb1e252
Lukas Holzer <office@lukasholzer.com>
2017-09-08 11:36:38 -0700

    add swiper

--- src/theme/index.php
+++ src/theme/index.php
@@ -8,5 +8,18 @@
 
   Timber::render( array( 'components/hero.twig' ), $header );
 
+  printf('<div class="container">');
+
+  if(have_posts()) : while(have_posts()) : the_post();
+
+    the_content();
+
+    Timber::render( array( 'components/swiper.twig' ), $header );
+
+
+  endwhile; endif;
+
+  printf('</div>');
+
   get_footer();
 ?>
