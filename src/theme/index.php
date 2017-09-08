afb95405adeeb9d9836c4fadf7ce9e21b701912b
Lukas Holzer <office@lukasholzer.com>
2017-09-08 11:18:05 -0700

    header on startpage

--- src/theme/index.php
+++ src/theme/index.php
@@ -1,5 +1,12 @@
-<?php get_header(); ?>
+<?php
 
+  get_header();
 
+  $header['style'] = array('hero--large');
+  $header['image'] = get_field('header_image');
+  $header['title'] = get_field('header_text');
 
-<?php get_footer(); ?>
+  Timber::render( array( 'components/hero.twig' ), $header );
+
+  get_footer();
+?>
