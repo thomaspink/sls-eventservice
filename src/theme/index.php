2699804b72e66dec6cc151a72d0c8429cd023048
Lukas Holzer <office@lukasholzer.com>
2017-09-10 11:55:09 -0700

    store pagination

--- src/theme/index.php
+++ src/theme/index.php
@@ -2,17 +2,20 @@
 
   get_header();
 
+  global $paged; // current page
+  if (!isset($paged) || !$paged){
+      $paged = 1;
+  }
 
   $args = array(
     'post_type' => 'post',
-    'posts_per_page' => 1,
-    'paged' => 1
+    'posts_per_page' => 2,
+    'paged' => $paged
 );
   $context = Timber::get_context();
   $context['posts'] = new Timber\PostQuery($args);
   $context['multiple'] = true;
   $template = array( 'pages/blog.twig' );
-
 
   Timber::render( $template, $context );
 
