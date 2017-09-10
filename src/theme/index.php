68b2d58f9daee437bebe62cb8ba31bfc7db530d6
Lukas Holzer <office@lukasholzer.com>
2017-09-09 17:45:53 -0700

    home blog overview

--- src/theme/index.php
+++ src/theme/index.php
@@ -6,6 +6,15 @@
   $header['image'] = get_field('header_image');
   $header['title'] = get_field('header_text');
 
+  $blog = array(
+    'post_type' => 'post',
+    'posts_per_page' => '3'
+  );
+
+  $context = Timber::get_context();
+  $context['posts'] = Timber::get_posts($blog);
+  $posts = array( 'components/post-overview.twig' );
+
   $swiper = array();
 
   Timber::render( array( 'components/hero.twig' ), $header );
@@ -21,6 +30,14 @@
   printf('</div>');
 
   Timber::render( array( 'components/swiper.twig' ), $swiper );
+
+  printf('<div class="container">');
+
+  printf('<h2>SLS – Blog</h2>');
+
+  Timber::render( $posts , $context );
+
+  printf('</div>');
 
   get_footer();
 ?>
