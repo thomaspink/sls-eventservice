5e8f862299edab8bf8ad8e4e4a385a2bca8fcc3e
Lukas Holzer <office@lukasholzer.com>
2017-09-17 16:38:18 +0200

    short updates

--- src/theme/templates/referenzen.php
+++ src/theme/templates/referenzen.php
@@ -10,6 +10,7 @@
 
   $context = Timber::get_context();
   $context['posts'] = new Timber\PostQuery($args);
+  $context['page'] = new Timber\Post();
   $template = array( 'pages/references.twig' );
 
   Timber::render( $template, $context );
