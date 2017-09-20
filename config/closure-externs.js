0ff49937fe2e3dd038cde760272103b017028660
Thomas Pink <thomas.pink@me.com>
2017-09-20 11:43:53 +0200

    switched to closure compiler

---
+++ config/closure-externs.js
@@ -0,0 +1,12 @@
+var webpackJsonp = function() {};
+var __g;
+var __e;
+var PromiseRejectionEvent;
+
+// All this is just for the swiper plugin that tests for the specific frameworks
+var jQuery;
+var Modernizr;
+var DocumentTouch;
+var define;
+var innerWidth;
+var innerHeight;
--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -2,7 +2,7 @@ const webpack = require('webpack');
 const webpackMerge = require('webpack-merge');
 const commonConfig = require('./webpack.common.js');
 const helpers = require('./helpers');
-const ManifestPlugin = require('webpack-manifest-plugin');
+const fs = require('fs');
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
@@ -10,6 +10,10 @@ const StyleExtHtmlWebpackPlugin = require('style-ext-html-webpack-plugin');
 const HtmlWebpackExcludeAssetsPlugin = require('html-webpack-exclude-assets-plugin');
 const ScriptExtHtmlWebpackPlugin = require('script-ext-html-webpack-plugin');
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const ManifestPlugin = require('webpack-manifest-plugin');
+const ClosureCompiler = require('google-closure-compiler-js').webpack;
+
+const externs = fs.readFileSync(helpers.root('config', 'closure-externs.js'), 'utf8');
 
 // const ENV = process.env.NODE_ENV = process.env.ENV = 'production';
 
@@ -54,13 +58,24 @@ module.exports = webpackMerge(commonConfig, {
       defaultAttribute: 'defer'
     }),
 
-    new webpack.optimize.UglifyJsPlugin({
-      comments: false,
-      dropDebugger: true,
-      dropConsole: true,
-      sourceMap: false,
-      compressor: {
-        warnings: false,
+    // new webpack.optimize.UglifyJsPlugin({
+    //   comments: false,
+    //   dropDebugger: true,
+    //   dropConsole: true,
+    //   sourceMap: false,
+    //   compressor: {
+    //     warnings: false,
+    //   },
+    // }),
+
+    new ClosureCompiler({
+      options: {
+        languageIn: 'ECMASCRIPT5',
+        languageOut: 'ECMASCRIPT5',
+        compilationLevel: 'ADVANCED',
+        warningLevel: 'VERBOSE',
+        // externs: [helpers.root('config', 'closure-externs.js')],
+        externs: [{ src: externs }]
       },
     }),
 
