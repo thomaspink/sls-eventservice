4c6cdd0ab425fb04ae401b7014fc6fb95796962a
Thomas Pink <thomas.pink@me.com>
2017-09-20 16:00:51 +0200

    image slider wip

--- src/client/app/components/image-slider.component.ts
+++ src/client/app/components/image-slider.component.ts
@@ -1,21 +1,37 @@
-import { Component, ELEMENT, ChildListener } from '../../core';
+import {Component, OnInit, OnDestroy, ChildListener, ViewChild} from '../../core';
+import {Swiper, SwiperRef} from '../services/swiper';
 
 @Component({
   selector: 'image-slider',
-  deps: [ELEMENT]
+  deps: [Swiper]
 })
-export class ImageSliderComponent {
-  constructor(private _el: Element) {
+export class ImageSliderComponent implements OnInit, OnDestroy {
+
+  private _swiperRef: SwiperRef;
+
+  @ViewChild('.image-slider-container')
+  private _container: Element;
+
+  constructor(private _swiper: Swiper) { }
+
+  onInit() {
+    if (this._swiperRef)
+      throw new Error(`Can not initialize image slider because there is already a swiper instance`);
+
+    this._swiperRef = this._swiper.create(this._container, {
+      loop: true,
+    });
+  }
+
+  onDestroy() {
+    if (this._swiperRef)
+      this._swiperRef.destroy();
   }
 
   @ChildListener('.image-slider-next', 'click')
-  next() {
-    // Todo @thomas.pink
-  }
+  next() { this._swiperRef.slideNext(); }
 
   @ChildListener('.image-slider-prev', 'click')
-  prev() {
-    // Todo @thomas.pink
-  }
+  prev() { this._swiperRef.slidePrev(); }
 
 }
