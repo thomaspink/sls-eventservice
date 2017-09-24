74979681716a3518c6d40ad960a8d50e89b591a1
Lukas Holzer <office@lukasholzer.com>
2017-09-24 19:56:08 +0200

    update refernces

--- src/theme/templates/referenzen.php
+++ src/theme/templates/referenzen.php
@@ -8,9 +8,16 @@
     'posts_per_page' => 4
   );
 
+  $gallery = array(
+    'post_type' => 'gallery',
+    'posts_per_page' => '-1'
+  );
+
+
   $context = Timber::get_context();
   $context['posts'] = new Timber\PostQuery($args);
   $context['page'] = new Timber\Post();
+  $context['gallery'] = Timber::get_posts($gallery);
   $template = array( 'pages/references.twig' );
 
   Timber::render( $template, $context );
