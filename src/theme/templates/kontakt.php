5c8519f461f62985c3f58d649ece6fb227a9f00a
Lukas Holzer <office@lukasholzer.com>
2017-09-26 11:16:32 +0200

    error logs

--- src/theme/templates/kontakt.php
+++ src/theme/templates/kontakt.php
@@ -3,6 +3,11 @@
    * Template Name: Kontakt
    */
 
+    use theme\controller as controller;
+
+    $mail = new controller\MailController();
+
+
     $context = Timber::get_context();
     $context['page'] = new Timber\Post();
     Timber::render( array( 'pages/contact.twig' ), $context );
